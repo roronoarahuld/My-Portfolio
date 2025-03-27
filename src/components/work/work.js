33d8df5673483832a9ad3d2b26173a0d491a278a
Rahul Dolui <rahul.dolui@homevillegroup.com>
2025-03-27 19:31:00 +0530

    animation work

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -9,23 +9,24 @@ export default function Work() {
                     <div className="row">
                         <div className="col-12">
                             {/* <div className={isExpanded === true ? "content-section remove-abs" : "content-section"}> */}
-                            <div className="content-section">
+                                <div className="content-section">
                                 <h1>Transforming Ideas into Impactful Designs</h1>
                                 {/* Slider */}
-                                <div className="carousel">
-                                    <div className="slides">
-                                        <div id="slide-1" className={isExpanded === true ? "box expanded" : "box"} onClick={() => setIsExpanded(true)}>
-                                            1
-                                        </div>
-                                        <div id="slide-2" className="box">2</div>
-                                        <div id="slide-3" className="box">3</div>
-                                        <div id="slide-4" className="box">4</div>
-                                        <div id="slide-5" className="box">5</div>
-                                        <div id="slide-6" className="box">6</div>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="carousel">
+                    <div className="slides">
+                        <div id="slide-1" className={isExpanded === true ? "box expanded" : "box"} onClick={() => setIsExpanded(true)}>
+                            1
+                        </div>
+                        <div id="slide-2" className="box">2</div>
+                        <div id="slide-3" className="box">3</div>
+                        <div id="slide-4" className="box">4</div>
+                        <div id="slide-5" className="box">5</div>
+                        <div id="slide-6" className="box">6</div>
                     </div>
                 </div>
             </section>
