930e54b13fdd7391bbb0cd995ffe4a6449761770
Rahul Dolui <rahul.dolui@homevillegroup.com>
2025-04-09 19:53:23 +0530

    contact api integration done

--- src/commonComponents/Offcanvas.js
+++ src/commonComponents/Offcanvas.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useApiData } from "../services/Api";
 type Props = {
     Attribute: string,
     title: string,
@@ -11,55 +11,54 @@ type Props = {
 }
 
 export const ContactOffCanvas = () => {
+    const { storeData } = useApiData()
     return (
         <div className="offcanvas offcanvas-end " tabIndex="-1" id="contactSection" aria-labelledby="offcanvasBottomLabel">
             <div className="offcanvas-body small">
                 <div className="contact_div">
-                    <h3>Let's Connect</h3>
-                    <div className="social">
-                        <span>Social Media</span>
-                        <ul>
-                            <li>
-                                <a href="#!">
-                                    <span>Facebook</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#!">
-                                    <span>Instagram</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#!">
-                                    <span>LinkedIn</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#!">
-                                    <span>Github</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#!">
-                                    <span>Youtube</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="contact">
-                        <span>Contact</span>
-                        <ul>
-                            <li>
-                                <a href="#!">
-                                    <span>Email</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#!">
-                                    <span>Contact</span>
-                                </a>
-                            </li>
-                        </ul>
+                    <h3>{storeData[0]?.contactPage[0]?.title}</h3>
+                    <div>
+                        {
+                            storeData[0]?.contactPage[0]?.descp.map((links) => {
+                                return (
+                                    <>
+                                        <span>{links?.title}</span>
+                                        <ul>
+                                            {
+                                                links?.links?.map((url) => {
+                                                    return (
+                                                        <li key={url}>
+                                                            <a href={url?.link} target="_blank">
+                                                                <span className="img-border">
+                                                                    {
+                                                                        url?.linkName === "Github" ? (
+                                                                            <img src={require("../images/github.svg").default} />
+                                                                        ) : url?.linkName === "LinkedIn" ? (
+                                                                            <img src={require("../images/linkedin.svg").default} />
+                                                                        ) : url?.linkName === "Facebook" ? (
+                                                                            <img src={require("../images/facebook.svg").default} />
+                                                                        ) : url?.linkName === "Instagram" ? (
+                                                                            <img src={require("../images/instagram.svg").default} />
+                                                                        ) : url?.linkName === "Youtube" ? (
+                                                                            <img src={require("../images/youtube.svg").default} />
+                                                                        ) : url?.linkName === "Phone" ? (
+                                                                            <img src={require("../images/phone-call.svg").default} />
+                                                                        ) : (
+                                                                            <img src={require("../images/envelope.svg").default} />
+                                                                        )
+                                                                    }
+                                                                </span>
+                                                                <span>{url?.linkName}</span>
+                                                            </a>
+                                                        </li>
+                                                    )
+                                                })
+                                            }
+                                        </ul>
+                                    </>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
@@ -100,8 +99,8 @@ export const SliderOffCanvas = ({ Attribute, title, projectHandled, subtitle, we
                     <div className="row">
                         <div className="col-12 mb-5 d-flex justify-content-center flex-wrap">
                             {
-                                projectImage[0]?.images?.map((primg)=>{
-                                    return(
+                                projectImage[0]?.images?.map((primg) => {
+                                    return (
                                         <img key={primg} src={primg?.link} className="img-fluid" />
                                     )
                                 })
@@ -114,8 +113,8 @@ export const SliderOffCanvas = ({ Attribute, title, projectHandled, subtitle, we
                         </div>
                         <div className="col-md-6 d-flex justify-content-center flex-wrap">
                             {
-                                projectImage[1]?.images?.map((primg)=>{
-                                    return(
+                                projectImage[1]?.images?.map((primg) => {
+                                    return (
                                         <img key={primg} src={primg?.link} className="img-fluid" />
                                     )
                                 })
@@ -125,15 +124,15 @@ export const SliderOffCanvas = ({ Attribute, title, projectHandled, subtitle, we
                     <div className="row mt-5">
                         <div className="col-md-6">
                             {
-                                projectImage[2]?.images?.map((primg)=>{
-                                    return(
+                                projectImage[2]?.images?.map((primg) => {
+                                    return (
                                         <img key={primg} src={primg?.link} className="img-fluid" />
                                     )
                                 })
                             }
                         </div>
                         <div className="col-md-6">
-                        <p>{projectImage[2]?.description}</p>
+                            <p>{projectImage[2]?.description}</p>
                         </div>
                     </div>
                 </div>
